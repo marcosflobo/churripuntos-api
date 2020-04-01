6db4053f3152e5b19ea80badfdd7c1007d856c92
Marcos F. Lobo <lobo@lukos.org>
2020-04-01 19:51:01 +0200

    Initial commit with micronaut project

---
+++ src/main/java/org/lukos/Application.java
@@ -0,0 +1,18 @@
+package org.lukos;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "churripuntos-api",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
